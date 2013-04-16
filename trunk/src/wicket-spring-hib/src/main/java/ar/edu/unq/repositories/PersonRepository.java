64b8ed210c180081068fc159aa34c5f8114c90e2
cristianelopez <cristianelopez@48cc0aa2-1ce8-4d3d-0310-f4ba6f7e7943>
2013-04-16 21:05:53 +0000

    cambios que no me acuerdo

--- trunk/src/wicket-spring-hib/src/main/java/ar/edu/unq/repositories/PersonRepository.java
+++ trunk/src/wicket-spring-hib/src/main/java/ar/edu/unq/repositories/PersonRepository.java
@@ -11,29 +11,24 @@ import org.springframework.orm.hibernate3.HibernateCallback;
 
 import ar.edu.unq.model.Person;
 
-public class PersonRepository extends HibernateGenericDAO<Person> implements
-		GenericRepository<Person> {
+public class PersonRepository extends HibernateGenericDAO<Person> implements GenericRepository<Person> {
 
-	private static final long serialVersionUID = -4036535812105672110L;
+    private static final long serialVersionUID = -4036535812105672110L;
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public List<Person> filterPeople(final String pattern) {
-		return (List<Person>) this.getHibernateTemplate().execute(
-				new HibernateCallback() {
-					@Override
-					public List<Person> doInHibernate(final Session session)
-							throws HibernateException, SQLException {
-						Criteria criteria = session
-								.createCriteria(Person.class);
-						criteria.add(Restrictions.like("name", "%" + pattern
-								+ "%"));
-						return criteria.list();
-					}
-				});
-	}
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public List<Person> filterPeople(final String pattern) {
+        return (List<Person>) this.getHibernateTemplate().execute(new HibernateCallback() {
+            @Override
+            public List<Person> doInHibernate(final Session session) throws HibernateException, SQLException {
+                Criteria criteria = session.createCriteria(Person.class);
+                criteria.add(Restrictions.like("name", "%" + pattern + "%"));
+                return criteria.list();
+            }
+        });
+    }
 
-	@Override
-	protected Class<Person> getDomainClass() {
-		return Person.class;
-	}
+    @Override
+    protected Class<Person> getDomainClass() {
+        return Person.class;
+    }
 }
